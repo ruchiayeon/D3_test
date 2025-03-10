5e44fb11ba20118dd680a7066d3955e9175e75af
r7 <r7@smtechnology.kr>
2025-03-10 10:28:15 +0900

    [FEAT] LineChart 추가가

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import LineChart from "./HistogramWithByteEntropy";
 
 import { Provider } from "react-redux";
 import store from "./Redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
-    <Provider store={store}>
-        <StrictMode>
-            <App />
-        </StrictMode>
-    </Provider>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+      <LineChart />
+    </StrictMode>
+  </Provider>
 );
